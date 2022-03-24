035bd1dd05b54127def5c8bb739d3532d120bfbf
patrycjamoss <patrycjamoss@gmail.com>
2022-03-24 14:43:54 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log(`WARSZTAT- Obsługa forlularza 'Imie i Nazwisko'`)
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name=lname]');
+
+    console.log(`Imie:${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submitForm);
+
